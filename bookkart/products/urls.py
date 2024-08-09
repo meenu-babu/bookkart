761e7614536d884edf3d116bf8207a6a0ec193ea
meenu-babu <bmeenu1995@gmail.com>
2024-08-09 13:24:59 +0530

    bookkart ecommerce website

---
+++ bookkart/products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+
+urlpatterns = [
+    path('', views.index,name='home'),
+    path('list_products', views.product_list,name='product_list'),
+     path('detail_products', views.product_details,name='product_details')
+]
